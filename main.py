0293a4d29e8695bac86fc71753783a4e09f15b0e
MKDBGNS <154989220+MKDBGNS@users.noreply.github.com>
2025-07-20 23:22:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters, idle
 
-client = Client("mp", api_id=…, api_hash=…, bot_token=…)
+client = Client("mp", api_id=20014140, api_hash=548622ea394cf4328c47bfd0d45419a0, bot_token=7890433682:AAETDYhwbue9k0nhGhBaxJSbeXRBX7yr_9s)
 
 @client.on_message(filters.command("ping"))
 async def ping(c, m):
